81a012e55c2f368642bbaa0739a221ea9b7699a3
Diego Prieto <da.prieto1@escuelaing.edu.co>
2020-04-01 19:31:38 -0500

    challenge to avoid to many round trips in findAll sites

--- src/daos/impl/redis/site_geo_dao_redis_impl.js
+++ src/daos/impl/redis/site_geo_dao_redis_impl.js
@@ -114,17 +114,16 @@ const findAll = async () => {
   const siteIds = await client.zrangeAsync(keyGenerator.getSiteGeoKey(), 0, -1);
   const sites = [];
 
+  const pipeline = client.batch();
   for (const siteId of siteIds) {
     const siteKey = keyGenerator.getSiteHashKey(siteId);
+    pipeline.hgetallAsync(siteKey);
+  }
+  const resultSites = await pipeline.execAsync();
 
-    /* eslint-disable no-await-in-loop */
-    const siteHash = await client.hgetallAsync(siteKey);
-    /* eslint-enable */
-
-    if (siteHash) {
-      // Call remap to remap the flat key/value representation
-      // from the Redis hash into the site domain object format.
-      sites.push(remap(siteHash));
+  for (const site of resultSites) {
+    if (site) {      
+      sites.push(remap(site));
     }
   }
 
